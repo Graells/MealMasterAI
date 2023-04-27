07dcb75173a371463328b2a7f617db06469e01d6
Maria <igoninamv@yandex.ru>
2023-04-27 20:13:43 +0200

    test: finished one component

--- client/src/tests/ShareButton.test.js
+++ client/src/tests/ShareButton.test.js
@@ -1,16 +1,22 @@
-// import React from 'react'
-// import { render, screen } from '@testing-library/react'
-// import '@testing-library/jest-dom'
-// import ShareButton from 'react-share/lib/ShareButton';
+import React from "react";
+import {
+  render,
+  fireEvent
+} from "@testing-library/react";
+import ShareButton from "./ShareButton";
 
-// test('Share button', () => {
-  
-// })
-
-// describe('Spinner', () => {
-//   it('Should be shown when the info is loading', () => {
-//     const { container } = render(<Spinner />);
-//     const spinnerDiv = container.querySelector('.spinner');
-//     expect(spinnerDiv).toBeInTheDocument();
-//   });
-// });
+describe("ShareButton", () => {
+  test("clicking on the WhatsApp share button should open the WhatsApp share dialog with the correct message", () => {
+    const diet = {
+      id: "1",
+      mealInfo: {
+        title: "Test Diet Plan"
+      },
+      description: "This is a test diet plan"
+    };
+    const { getByTestId } = render(<ShareButton diet={diet} />);
+    const whatsappShareButton = getByTestId("whatsapp-share-button");
+    fireEvent.click(whatsappShareButton);
+    expect(window.location.href).toContain(`whatsapp://send?text=Check%20out%20this%20diet%20plan%20created%20using%20AI%20tech%3A%20${encodeURIComponent(diet.mealInfo.title)}`);
+  });
+})
