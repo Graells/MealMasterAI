a90c4b5ef1b359035bba405f0b2c87815ef56491
Raul Barros <raulhbarros@outlook.com>
2023-04-27 13:00:32 +0200

    added new questions to Schema

---
+++ server/controller.js
@@ -0,0 +1,134 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const dotenv = require('dotenv');
+dotenv.config();
+const { OpenAIApi, Configuration } = require('openai');
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+);
+
+const controller = {};
+
+controller.getAll = async (req, res) => {
+  try {
+    const meals = await prisma.mealAI.findMany({
+      include: {
+        user: true,
+        mealInfo: true,
+      },
+    });
+    res.status(200).json(meals);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching meals' });
+    console.log(error);
+  }
+};
+controller.postAI = async (req, res) => {
+  const {
+    auth0Id,
+    userName,
+    userPic,
+    email,
+    title,
+    name,
+    age,
+    gender,
+    weight,
+    height,
+    activityLevel,
+    howManyDays,
+    howMuchMoney,
+    dietaryPreferences,
+    weightGoal,
+    weightAmount,
+    timeFrame,
+    eatingFrequency,
+  } = req.body;
+  try {
+    const prompt = `Generate a diet plan for user name ${name}, a ${age}-year-old ${gender}, weighing ${weight} kg, and ${height} cm tall, with an activity level of ${activityLevel}, how many ${howManyDays} the user needs a diet for, spending this much ${howMuchMoney} per day, dietary preferences of ${dietaryPreferences}, a weight goal of ${weightGoal} ${weightAmount} kg, a time frame of ${timeFrame} weeks, and an eating frequency of ${eatingFrequency} times a day.`;
+    console.log(prompt);
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: `${prompt}. Give the response as if you were talking to the user directly saying his/her name.`,
+        },
+      ],
+    });
+    const diet = response.data.choices[0].message.content;
+    // res.json(diet);
+    const user = await prisma.user.upsert({
+      where: { auth0Id },
+      update: {},
+      create: { auth0Id, email: email, userName, userPic },
+    });
+    const newMealInfo = await prisma.mealInfo.create({
+      data: {
+        title,
+        name,
+        age,
+        gender,
+        weight,
+        height,
+        activityLevel,
+        howManyDays,
+        howMuchMoney,
+        dietaryPreferences,
+        weightGoal,
+        weightAmount,
+        timeFrame,
+        eatingFrequency,
+      },
+    });
+    const newMealAI = await prisma.mealAI.create({
+      data: {
+        description: diet,
+        userId: user.auth0Id,
+        mealInfoId: newMealInfo.id,
+      },
+    });
+    res.json({ ...newMealAI, user, mealInfo: newMealInfo });
+  } catch (error) {
+    console.error('Error generating diet plan:', error);
+    res.status(500).json({ error: 'Failed to generate diet plan' });
+  }
+};
+
+controller.getOne = async (req, res) => {
+  try {
+    const meal = await prisma.mealInfo.findUnique({
+      where: { id: parseInt(req.params.id) },
+    });
+    if (!meal) return res.status(404).json({ error: 'Meal not found' });
+    res.status(200).json(meal);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching meal' });
+    console.log(error);
+  }
+};
+controller.updateOne = async (req, res) => {
+  try {
+    const { title } = req.body;
+    const updatedMeal = await prisma.mealInfo.update({
+      where: { id: parseInt(req.params.id) },
+      data: { title },
+    });
+    res.status(200).json(updatedMeal);
+  } catch (error) {
+    res.status(500).json({ error: 'Error updating meal' });
+  }
+};
+
+controller.deleteOne = async (req, res) => {
+  try {
+    await prisma.mealAI.delete({ where: { id: parseInt(req.params.id) } });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: 'Error deleting meal' });
+  }
+};
+
+module.exports = controller;
