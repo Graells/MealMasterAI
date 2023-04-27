a638591111cb3773b920ddaeaceda03c8cb2055a
Jordan Rollins <carpeomniavino@gmail.com>
2023-04-27 13:15:23 +0200

    index.ts types added

--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,28 @@
 const express = require('express');
-require('dotenv').config();
+const dotenv = require('dotenv');
 const { PrismaClient } = require('@prisma/client');
 const router = require('./router');
-var cors = require('cors')
+const cors = require('cors');
+
+dotenv.config();
 const app = express();
 const prisma = new PrismaClient();
-
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
+const PORT: number = Number(process.env.PORT) || 3001;
 
-const PORT = process.env.PORT || 3001;
 (async () => {
   try {
     await prisma.$connect();
-    app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server listening on: http://localhost:${PORT}`)
+    );
   } catch (error) {
     console.log('Error in connecting to database :', error);
   }
 })();
+
+export {};
