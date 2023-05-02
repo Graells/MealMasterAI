6d4e3e9cda15a2a30eece0208e41fda6d44015b7
Raul Barros <raulhbarros@outlook.com>
2023-05-02 12:38:31 +0200

    home page/ profile almost done

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Profile from "./Profile";
+import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <>
+    <div className="footer">
       <Profile />
-    </>
+    </div>
   );
 };
 
